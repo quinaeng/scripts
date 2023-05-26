f23ebedfe93ce2ffa3470d8da92be97539799f28
quinaeng <quinaeng@gmail.com>
2023-05-27 07:41:24 +0900

    コメントアウト機能追加

--- str_replace/str_replace.sh
+++ str_replace/str_replace.sh
@@ -5,9 +5,15 @@ REPLACE_WORD="./params.txt"
 
 cd $(dirname $0)
 
+
 # 文字列置換を行う対象のファイルパスを取得
 while read file_path
 do
+
+  # コメントアウト機能
+  if [[ $file_path == \#* ]]; then
+	continue
+  fi
 
   # file_listのファイル有無を確認
   if [ -e "$file_path" ]; then
@@ -15,12 +21,28 @@ do
     # 置換前と置換後の文字を取得
     while read replace_word
     do
+
+	  # コメントアウト機能
+	  if [[ $replace_word == \#* ]]; then
+		continue
+	  fi
+
       before_word=$(echo $replace_word | cut -d":" -f1)
       after_word=$(echo $replace_word | cut -d":" -f2)
 
+	  grep -q "$before_word" $file_path
+	  bw_exists=$?
+
       # 文字列を置換する
-      sed -i "s/$before_word/$after_word/g" $file_path
-      echo "[OK]: $file_path"
+      sed -i 's|'"$before_word"'|'"$after_word"'|g' $file_path
+
+	  # ログ出力
+      if [ $bw_exists = 0 ]; then
+    	echo "File Path: $file_path" | tee -a result.txt
+    	echo "Before word: $before_word" | tee -a result.txt
+    	echo "After word: $after_word" | tee -a result.txt
+		echo "---" | tee -a result.txt
+      fi
 
     done < $REPLACE_WORD
   else
